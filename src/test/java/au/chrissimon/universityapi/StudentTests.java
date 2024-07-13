fcbe2fa254c5badf4592ee15c084ada31e063455
박혜민/백엔드개발팀 <hyemin@ktown4u.com>
2024-07-13 22:34:59 +0900

    1R

---
+++ src/test/java/au/chrissimon/universityapi/StudentTests.java
@@ -0,0 +1,34 @@
+package au.chrissimon.universityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
+import static org.springframework.test.web.reactive.server.WebTestClient.bindToServer;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StudentTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void givenIAmAStudent_WhenIRegister() {
+        final WebTestClient.ResponseSpec response = bindToServer()
+                .baseUrl("http://localhost:" + port)
+                .build()
+                .post()
+                .uri("/student")
+                .exchange();
+
+        itShouldRegisterANewStudent(response);
+    }
+
+    private WebTestClient.ResponseSpec itShouldRegisterANewStudent(final WebTestClient.ResponseSpec response) {
+        return response
+                .expectStatus()
+                .isCreated();
+    }
+}
