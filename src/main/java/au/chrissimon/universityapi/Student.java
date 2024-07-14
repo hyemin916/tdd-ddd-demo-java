b2550f8740c02acf3a742e45d5108c2c5620b7ff
박혜민/백엔드개발팀 <hyemin@ktown4u.com>
2024-07-14 22:48:53 +0900

    4G

--- src/main/java/au/chrissimon/universityapi/Student.java
+++ src/main/java/au/chrissimon/universityapi/Student.java
@@ -2,8 +2,8 @@ package au.chrissimon.universityapi;
 
 import java.util.UUID;
 
-public record Student(UUID id) {
-    public static Student register(final UUID uuid) {
-        return new Student(uuid);
+public record Student(UUID id, String name) {
+    public static Student register(final String name) {
+        return new Student(UUID.randomUUID(), name);
     }
 }
--- src/main/java/au/chrissimon/universityapi/StudentController.java
+++ src/main/java/au/chrissimon/universityapi/StudentController.java
@@ -2,18 +2,18 @@ package au.chrissimon.universityapi;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.UUID;
 
 @RestController
 public class StudentController {
 
     @PostMapping("/students")
-    ResponseEntity<Student> registerNewStudent() {
-        final Student newStudent = Student.register(UUID.randomUUID());
+    ResponseEntity<Student> registerNewStudent(@RequestBody final Student registerStudentRequest) {
+        final Student newStudent = Student.register(registerStudentRequest.name());
         final URI newStudentLocation = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
